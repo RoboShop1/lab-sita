79a3e4c6f659b60b8a541df4dbffc08a6bf92e0c
chaithanya <chaithanya1812@gmail.com>
2025-03-22 11:51:45 +0530

    ss

---
+++ env-prod/ec2.auto.tfvars
@@ -0,0 +1,6 @@
+ec2 = {
+  instance1 = {
+    ami_id        = "ami-08b5b3a93ed654d19"
+    instance_type = "t2.micro"
+  }
+}
--- run.sh
+++ run.sh
@@ -13,9 +13,22 @@ case $1 in
       	export TF_VAR_region=ap-south-1
       	terraform plan
       	terraform apply
+      	rm -f .terraform/terraform.tfstate
     ;;
   "prod")
-    echo "You selected option 2"
+        	rm -rf *auto.tfvars credentials
+        	AWS_ROLE_ARN=$(cat .aws/accounts.json | jq -r ".prod.arn" )
+        	aws sts assume-role --role-arn $AWS_ROLE_ARN --role-session-name prod | tee credentials
+        	echo "Assumed role: $AWS_ROLE_ARN "
+        	export AWS_ACCESS_KEY_ID=$(jq -r '.Credentials.AccessKeyId' < credentials)
+        	export AWS_SECRET_ACCESS_KEY=$(jq -r '.Credentials.SecretAccessKey' < credentials)
+        	export AWS_SESSION_TOKEN=$(jq -r '.Credentials.SessionToken' < credentials)
+        	cp env-dev/*.auto.tfvars .
+        	terraform init -backend-config=env-dev/state.tfvars
+        	export TF_VAR_env=dev
+        	export TF_VAR_region=ap-south-1
+        	terraform plan
+        	terraform apply
     ;;
   *)
     echo "Invalid option"
