8ea62398a702938340f04bdba44168837a293193
chaithanya <chaithanya1812@gmail.com>
2025-03-28 21:05:34 +0530

    ss

--- Eks-Main/main.tf
+++ Eks-Main/main.tf
@@ -11,39 +11,39 @@ module "vpc" {
 
 
 
-resource "aws_instance" "main" {
-  for_each       =  lookup({for i,j in module.vpc["main"]: i => {for m,n in j: m => n.id } },"public_subnets", null)
-  ami            =  "ami-0b4f379183e5706b9"
-  instance_type  = "t2.micro"
-  subnet_id      = each.value
-
-
-  tags = {
-    Name = "Instance-${each.key}"
-  }
-}
-
-resource "aws_instance" "app" {
-  for_each       =  lookup({for i,j in module.vpc["main"]: i => {for m,n in j: m => n.id } },"app_subnets", null)
-  ami            =  "ami-0b4f379183e5706b9"
-  instance_type  = "t2.micro"
-  subnet_id      = each.value
-
-  tags = {
-    Name = "Instance-${each.key}"
-  }
-}
-
-resource "aws_instance" "db" {
-  for_each       =  lookup({for i,j in module.vpc["main"]: i => {for m,n in j: m => n.id } },"db_subnets", null)
-  ami            =  "ami-0b4f379183e5706b9"
-  instance_type  = "t2.micro"
-  subnet_id      = each.value
-
-  tags = {
-    Name = "Instance-${each.key}"
-  }
-}
+# resource "aws_instance" "main" {
+#   for_each       =  lookup({for i,j in module.vpc["main"]: i => {for m,n in j: m => n.id } },"public_subnets", null)
+#   ami            =  "ami-0b4f379183e5706b9"
+#   instance_type  = "t2.micro"
+#   subnet_id      = each.value
+#
+#
+#   tags = {
+#     Name = "Instance-${each.key}"
+#   }
+# }
+#
+# resource "aws_instance" "app" {
+#   for_each       =  lookup({for i,j in module.vpc["main"]: i => {for m,n in j: m => n.id } },"app_subnets", null)
+#   ami            =  "ami-0b4f379183e5706b9"
+#   instance_type  = "t2.micro"
+#   subnet_id      = each.value
+#
+#   tags = {
+#     Name = "Instance-${each.key}"
+#   }
+# }
+#
+# resource "aws_instance" "db" {
+#   for_each       =  lookup({for i,j in module.vpc["main"]: i => {for m,n in j: m => n.id } },"db_subnets", null)
+#   ami            =  "ami-0b4f379183e5706b9"
+#   instance_type  = "t2.micro"
+#   subnet_id      = each.value
+#
+#   tags = {
+#     Name = "Instance-${each.key}"
+#   }
+# }
 
 
 output "main" {
