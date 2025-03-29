4c70a68607adcfae6588fdfa6f23e9e8407c82e3
chaithanya <chaithanya1812@gmail.com>
2025-03-29 10:21:10 +0530

    ss

--- Eks-Main/modules/eks/addons.tf
+++ Eks-Main/modules/eks/addons.tf
@@ -1,9 +1,9 @@
-# resource "aws_eks_addon" "addons" {
-#
-#   depends_on                  = [aws_eks_node_group.main]
-#   for_each                    =  var.addons
-#   cluster_name                = aws_eks_cluster.example.name
-#   addon_name                  = each.key
-#   addon_version               = each.value["addon_version"]
-#   resolve_conflicts_on_update = "OVERWRITE"
-# }
+resource "aws_eks_addon" "addons" {
+
+  depends_on                  = [aws_eks_node_group.main]
+  for_each                    =  var.addons
+  cluster_name                = aws_eks_cluster.example.name
+  addon_name                  = each.key
+  addon_version               = each.value["addon_version"]
+  resolve_conflicts_on_update = "OVERWRITE"
+}
