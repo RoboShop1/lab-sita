85b5c5f725d3ac2b7026aecb0b3abf5fab3499de
chaithanya <chaithanya1812@gmail.com>
2025-03-31 08:20:28 +0530

    endpoint

--- labs/endpoints/main.tf
+++ labs/endpoints/main.tf
@@ -6,14 +6,16 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id             = data.aws_vpc.default.id
   service_name       = "com.amazonaws.us-west-1.ec2"
   subnet_ids         = ["subnet-0e4185a248d6d0b4b"]
+  vpc_endpoint_type  = "Interface"
 
 }
 
 resource "aws_instance" "app" {
-  ami            =  "ami-0b4f379183e5706b9"
-  instance_type  = "t2.micro"
-  subnet_id      = "subnet-0843f25967fb0b18d"
+  ami             =  "ami-0b4f379183e5706b9"
+  instance_type   = "t2.micro"
+  subnet_id       = "subnet-0843f25967fb0b18d"
   security_groups = ["sg-00a30a397c35892b0"]
+
   tags = {
     Name = "Instance"
   }
