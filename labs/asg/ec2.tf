8ae078549378bda4d481af34f44ae75f7b3bcabf
chaithanya <chaithanya1812@gmail.com>
2025-04-02 20:55:31 +0530

    rds

--- labs/asg/ec2.tf
+++ labs/asg/ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "main" {
 
 
 
-resource "aws_instance" "main" {
+resource "aws_instance" "main1" {
   launch_template {
     version = "2"
     id      = "lt-059250556e7f305e4"
