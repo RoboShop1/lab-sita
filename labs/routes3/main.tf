1d98895fbd2b2eb782705449edb467f4cee72e1a
chaithanya <chaithanya1812@gmail.com>
2025-04-03 15:28:47 +0530

    rds

--- labs/routes3/main.tf
+++ labs/routes3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "record-internal" {
-  name    = "sample-internal.poornachandra3.online"
+  name    = "sample-internal"
   type    = "A"
   zone_id = "Z06926562JRB6HEXD0QMM"
   records = ["10.0.0.0"]
