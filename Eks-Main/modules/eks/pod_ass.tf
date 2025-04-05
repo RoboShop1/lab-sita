ee1ee9a6ddf3c23047b5e28085543a373b32a48f
chaithanya <chaithanya1812@gmail.com>
2025-04-05 09:30:27 +0530

    ss

---
+++ Eks-Main/modules/eks/pod_ass.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_role" "pod-associations-roles" {
+  for_each           =  var.aws_eks_pod_identity_associations
+  name               = each.key
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+      }
+    ]
+    Version = "2012-10-17"
+  })
+  inline_policy {
+    name   = each.key["policy_name"]
+    policy = jsonencode(each.value["policy"])
+  }
+}
+
+
+resource "aws_eks_pod_identity_association" "aws_eks_pod_asa" {
+  for_each        = aws_iam_role.pod-associations-roles
+  cluster_name    = aws_eks_cluster.example.name
+  namespace       = lookup(lookup(var.aws_eks_pod_identity_associations,each.key,null),"namespace",null)
+  service_account = lookup(lookup(var.aws_eks_pod_identity_associations,each.key,null),"service_account ",null)
+  role_arn        = each.value.arn
+}
