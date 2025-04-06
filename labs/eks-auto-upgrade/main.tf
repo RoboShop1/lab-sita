3ede5537e29a9982e42ada1a7a8ace118ddb5409
chaithanya <chaithanya1812@gmail.com>
2025-04-06 22:59:04 +0530

    ss

--- labs/eks-auto-upgrade/main.tf
+++ labs/eks-auto-upgrade/main.tf
@@ -7,9 +7,11 @@ module "eks" {
   node_polices    = each.value["node_polices"]
   node_group_1a   = each.value["node_group_1a"]
   node_group_1b   = each.value["node_group_1b"]
-  cluster_upgrade = each.value["cluster_upgrade"]
+  cluster_upgrade =  var.cluster_upgrade
 }
 
+
+variable "cluster_upgrade" {}
 
 variable "eks" {
   default = {
@@ -23,7 +25,6 @@ variable "eks" {
       ]
       node_group_1a    = {subnets = ["subnet-0843f25967fb0b18d","subnet-0354194ae815795f6"], instance_types= ["t3.small"], desired_size = "2" ,max_size= "3", min_size = "1"}
       node_group_1b    = {subnets = ["subnet-0843f25967fb0b18d","subnet-0354194ae815795f6"], instance_types= ["t3.small"], desired_size = "2" ,max_size= "3", min_size = "1"}
-      cluster_upgrade  = false
     }
   }
 }
