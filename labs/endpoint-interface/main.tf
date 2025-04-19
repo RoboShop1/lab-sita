8f6224e30edea590ddc9c4f58dfd0be1b7de98bd
chaithanya <chaithanya1812@gmail.com>
2025-04-19 16:25:06 +0530

    ss

--- labs/endpoint-interface/main.tf
+++ labs/endpoint-interface/main.tf
@@ -1,3 +1,4 @@
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -11,9 +12,39 @@ resource "aws_vpc_endpoint" "s3" {
   security_group_ids = [
     "sg-00a30a397c35892b0"
   ]
-
-
   tags = {
     Name = "s3-endpoint"
+  }
+}
+
+
+
+resource "aws_security_group" "ec2-sg" {
+  name = "ec2-sg"
+  description = "Allow ec2"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 22
+    protocol = "-1"
+  }
+
+}
+
+resource "aws_instance" "main" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  subnet_id = "subnet-0843f25967fb0b18d"
+  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
+
+  tags = {
+    Name = "one"
   }
 }
