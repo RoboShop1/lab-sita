39e0b9173d9e6403dda8bcdd6fc5f58569727641
chaithanya <chaithanya1812@gmail.com>
2025-04-19 18:11:45 +0530

    ss

--- labs/endpoint-interface/main.tf
+++ labs/endpoint-interface/main.tf
@@ -38,7 +38,7 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 
-resource "aws_vpc_endpoint" "s3" {
+resource "aws_vpc_endpoint" "sts" {
   vpc_id            = data.aws_vpc.default.id
   service_name      = "com.amazonaws.us-east-1.sts"
   vpc_endpoint_type = "Interface"
