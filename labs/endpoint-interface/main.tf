0e00de707b11e1aacbb00f650e034a5b5a3f4df3
chaithanya <chaithanya1812@gmail.com>
2025-04-19 16:13:04 +0530

    ss

---
+++ labs/endpoint-interface/main.tf
@@ -0,0 +1,16 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = data.aws_vpc.default.id
+  service_name      = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids       = ["subnet-0843f25967fb0b18d","subnet-0e4185a248d6d0b4b"]
+  security_group_ids = [
+    "sg-00a30a397c35892b0"
+  ]
+
+  private_dns_enabled = true
+}
