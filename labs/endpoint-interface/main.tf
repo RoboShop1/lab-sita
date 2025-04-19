4925b677015178baa40c9e47bd69c29f79a7204a
chaithanya <chaithanya1812@gmail.com>
2025-04-19 17:05:14 +0530

    ss

--- labs/endpoint-interface/main.tf
+++ labs/endpoint-interface/main.tf
@@ -32,9 +32,9 @@ resource "aws_security_group" "ec2-sg" {
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
