be7d8628bddd4cda625b51038786d8e420cdce37
chaithanya <chaithanya1812@gmail.com>
2025-04-19 18:07:16 +0530

    ss

--- labs/endpoint-interface/main.tf
+++ labs/endpoint-interface/main.tf
@@ -39,40 +39,44 @@ resource "aws_vpc_endpoint" "s3" {
 
 
 
-# resource "aws_security_group" "ec2-sg" {
-#   name = "ec2-sg"
-#   description = "Allow ec2"
-#   vpc_id = data.aws_vpc.default.id
-#
-#   ingress {
-#     from_port = 22
-#     to_port = 22
-#     protocol = "TCP"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-#
-# }
+resource "aws_security_group" "ec2-sg" {
+  name = "ec2-sg"
+  description = "Allow ec2"
+  vpc_id = data.aws_vpc.default.id
 
-# resource "aws_instance" "main" {
-#   ami = "ami-0b4f379183e5706b9"
-#   instance_type = "t3.small"
-#   subnet_id = "subnet-0843f25967fb0b18d"
-#   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
-#   iam_instance_profile = aws_iam_instance_profile.test_profile.name
-#
-#   tags = {
-#     Name = "one"
-#   }
-# }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "main" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  subnet_id = "subnet-0843f25967fb0b18d"
+  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name
+
+  tags = {
+    Name = "one"
+  }
+}
+
+output "two_ip" {
+  value = aws_instance.main.private_ip
+}
+
+
 # resource "aws_instance" "two" {
 #   ami = "ami-0b4f379183e5706b9"
 #   instance_type = "t3.small"
@@ -100,9 +104,7 @@ resource "aws_vpc_endpoint" "s3" {
 #
 #
 #
-# output "two_ip" {
-#   value = aws_instance.two.private_ip
-# }
+
 #
 #
 # output "three_ip" {
@@ -110,41 +112,44 @@ resource "aws_vpc_endpoint" "s3" {
 # }
 #
 #
-# resource "aws_iam_role_policy" "test_policy" {
-#   name = "test_policy"
-#   role = aws_iam_role.test_role.id
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "s3:*",
-#         ]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:DescribeParameters",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 #
-# resource "aws_iam_role" "test_role" {
-#   name = "test_role"
-#
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
-#
-# resource "aws_iam_instance_profile" "test_profile" {
-#   name = "test_profile"
-#   role = aws_iam_role.test_role.name
-# }
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = aws_iam_role.test_role.name
+}
