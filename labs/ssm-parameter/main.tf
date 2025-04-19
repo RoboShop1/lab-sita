69282712bda96d826da32e5f5a42cc7831b870c6
chaithanya <chaithanya1812@gmail.com>
2025-04-19 13:22:34 +0530

    ss

--- labs/ssm-parameter/main.tf
+++ labs/ssm-parameter/main.tf
@@ -51,7 +51,7 @@ resource "null_resource" "chaitu" {
 
 bash -c 'mkdir -p /tmp && echo "id = ${aws_iam_access_key.access-chaitu.id}" > /tmp/100.txt && echo "secret = ${aws_iam_access_key.access-chaitu.secret}" >> /tmp/100.txt'
 
-echo 'Hello-chaitu'
+"echo Hello-chaitu"
 EOT
   }
 }
