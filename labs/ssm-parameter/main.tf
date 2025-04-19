f42e5e87bd3cf5bc9d236d31f40dcacff5768db0
chaithanya <chaithanya1812@gmail.com>
2025-04-19 13:06:00 +0530

    ss

--- labs/ssm-parameter/main.tf
+++ labs/ssm-parameter/main.tf
@@ -44,8 +44,8 @@ resource "aws_iam_access_key" "access-chaitu" {
 resource "null_resource" "chaitu" {
   provisioner "local-exec" {
     commad = <<EOT
-"echo ${aws_iam_access_key.access-chaitu.id} >  100.txt"
-"echo ${aws_iam_access_key.access-chaitu.secret} >  100.txt"
+"echo id = ${aws_iam_access_key.access-chaitu.id} >  100.txt"
+"echo secret =  ${aws_iam_access_key.access-chaitu.secret} >>  100.txt"
 EOT
   }
 }
