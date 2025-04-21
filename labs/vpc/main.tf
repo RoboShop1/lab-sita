8651c826ea9c11c2427a2eddd53f7d6a46f1bb24
chaithanya <chaithanya1812@gmail.com>
2025-04-21 06:10:26 +0530

    ss

--- labs/vpc/main.tf
+++ labs/vpc/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "public" {
     from_port = 22
     to_port = 22
     protocol = "TCP"
-    cidr_blocks = ["O.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
