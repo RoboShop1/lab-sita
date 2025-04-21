76f8158e86d540b1aad2fd52a30e8981b1390488
chaithanya <chaithanya1812@gmail.com>
2025-04-21 09:07:55 +0530

    ss

--- labs/random-password/main.tf
+++ labs/random-password/main.tf
@@ -13,6 +13,22 @@ resource "random_string" "random1" {
 }
 
 
+
+resource "aws_secretsmanager_secret" "example" {
+  name = "example"
+}
+
+resource "aws_secretsmanager_secret_version" "example" {
+  secret_id     = aws_secretsmanager_secret.example.id
+  secret_string = jsonencode({
+    one = "${random_string.random.result}"
+    one1 = "${random_string.random1.result}"
+  })
+}
+
+
+
+
 output "one1" {
   value = random_string.random1.result
 }
