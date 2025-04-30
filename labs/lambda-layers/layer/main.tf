5a1e17ca665d88ec524d0c69f65f8174bdacaf83
chaithanya <chaithanya1812@gmail.com>
2025-04-30 20:46:15 +0530

    ss

--- labs/lambda-layers/layer/main.tf
+++ labs/lambda-layers/layer/main.tf
@@ -5,5 +5,5 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   s3_key              = "sample.zip"
   compatible_runtimes = ["python3.11"]
   compatible_architectures = ["x86_64"]
-  skip_destroy =  true
+  source_code_hash         = filebase64sha256("s3_key")
 }
