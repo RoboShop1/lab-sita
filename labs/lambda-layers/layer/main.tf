25a465207bdcca546ec4b283f2a877412105811c
chaithanya <chaithanya1812@gmail.com>
2025-04-30 21:15:34 +0530

    ss

--- labs/lambda-layers/layer/main.tf
+++ labs/lambda-layers/layer/main.tf
@@ -1,3 +1,18 @@
+data "aws_s3_object" "bootstrap_script" {
+  bucket = "goodchaitu"
+  key    = "sample.zip"
+}
+
+output "checksum" {
+  value = data.aws_s3_object.bootstrap_script.checksum_sha256
+}
+
+
+
+
+
+
+
 resource "aws_lambda_layer_version" "lambda_layer" {
   layer_name          = "two"
   description         = "My Python layer with requests"
@@ -7,3 +22,5 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   compatible_architectures = ["x86_64"]
   s3_object_version        = "sample.zip"
 }
+
+
