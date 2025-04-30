8b1c26e1c24885fc2cd07fa1113f65ca6ec56127
chaithanya <chaithanya1812@gmail.com>
2025-04-30 20:34:48 +0530

    ss

---
+++ labs/lambda-layers/layer/main.tf
@@ -0,0 +1,9 @@
+resource "aws_lambda_layer_version" "lambda_layer" {
+  layer_name          = "two"
+  description         = "My Python layer with requests"
+  s3_bucket           = "goodchaitu"
+  s3_key              = "layers/python-layer.zip"
+  compatible_runtimes = ["python3.11"]
+  compatible_architectures = ["x86_64"]
+  source_code_hash = filebase64sha256("python-layer.zip")
+}
