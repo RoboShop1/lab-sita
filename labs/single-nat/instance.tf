10a938589bec31679d2f6a881cb932b759bf8d17
chaithanya <chaithanya1812@gmail.com>
2025-06-09 09:53:06 +0530

    ss

--- labs/single-nat/instance.tf
+++ labs/single-nat/instance.tf
@@ -1,14 +1,24 @@
-#
-# resource "aws_instance" "nat-public" {
-#   ami            = "i-0817ad560e194335d"
-#   instance_type  = "t3.small"
-#   subnet_id      = aws_subnet.public.id
-#
-#
-#   tags = {
-#     Name = "nat-public"
-#   }
-# }
+
+resource "aws_instance" "nat-public" {
+  ami            = "i-0817ad560e194335d"
+  instance_type  = "t3.small"
+  subnet_id      = aws_subnet.public.id
 
 
-#
+  tags = {
+    Name = "nat-public-one"
+  }
+}
+
+
+resource "aws_instance" "dev-private" {
+  ami            = "i-0817ad560e194335d"
+  instance_type  = "t3.small"
+  subnet_id      = aws_subnet.dev-private.id
+
+
+  tags = {
+    Name = "dev-private-one"
+  }
+}
+
