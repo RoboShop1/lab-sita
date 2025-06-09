39480f1f6460c8501cf0ff656c45d148c92ab8dc
chaithanya <chaithanya1812@gmail.com>
2025-06-09 10:33:19 +0530

    ss

--- labs/single-nat/instance.tf
+++ labs/single-nat/instance.tf
@@ -10,8 +10,21 @@ resource "aws_instance" "nat-public" {
   }
 }
 
+resource "aws_instance" "nat-private" {
+  ami            = "ami-0b4f379183e5706b9"
+  instance_type  = "t3.small"
+  subnet_id      = aws_subnet.private.id
 
-#
+
+  tags = {
+    Name = "nat-private-one"
+  }
+}
+
+
+output "nat-private" {
+  value = aws_instance.nat-private.private_ip
+}
 
 output "nat-public-ip" {
   value = aws_instance.nat-public.public_ip
